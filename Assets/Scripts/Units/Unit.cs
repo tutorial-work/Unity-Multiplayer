41419503347f6657b6b14df9f73670f011285988
Buddhamon <wlacey@smu.edu>
2020-10-30 14:13:55 -0700

    Added stuff

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour
+{
+    /********** MARK: Private Variables **********/
+    #region Private Variables
+
+    [SerializeField] UnityEvent onSelected = null;
+    [SerializeField] UnityEvent onDeselected = null;
+
+    #endregion
+
+    /********** MARK: Server Functions **********/
+    #region Server Functions
+
+    #endregion
+
+    /********** MARK: Client Functions **********/
+    #region Client Functions
+    
+    public void Select()
+    {
+        if (!hasAuthority) return;
+
+        onSelected?.Invoke(); // ? mark checks if event is null "it's a safety check"
+    }
+
+    public void Deselect()
+    {
+        if (!hasAuthority) return;
+
+        onDeselected?.Invoke(); // ? mark checks if event is null "it's a safety check"
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UnitSelectionHandler : MonoBehaviour
+{
+    /********** MARK: Private Variables **********/
+    #region Private Variables
+
+    [SerializeField] LayerMask layerMask = new LayerMask();
+
+    Camera mainCamera;
+
+    List<Unit> selectedUnits = new List<Unit>();
+
+    #endregion
+
+    /********** MARK: Unity Functions **********/
+    #region Unity Functions
+
+    /// <summary>
+    /// Unity Method; Start() is called before the first frame update
+    /// </summary>
+    protected void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    /// <summary>
+    /// Unity Method; Update() is called once per frame
+    /// </summary>
+    protected void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            // Start selection area
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            ClearSelectionArea();
+        }
+    }
+
+    #endregion
+
+    /********** MARK: Class Functions **********/
+    #region Class Functions
+
+    private void ClearSelectionArea()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) return;
+
+        if (!hit.collider.TryGetComponent<Unit>(out Unit unit)) return;
+
+        if (!unit.hasAuthority) return;
+
+        selectedUnits.Add(unit);
+
+        foreach(Unit selectedUnit in selectedUnits)
+        {
+            selectedUnit.Select();
+        }
+    }
+
+    #endregion
+}
