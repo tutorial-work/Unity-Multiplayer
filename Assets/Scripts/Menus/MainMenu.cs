dd1ba92a73a6b696ac481c9f1ef3171beec742b8
Buddhamon <wlacey@smu.edu>
2020-11-19 11:41:50 -0800

    Steam Build Settings

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Mirror;
 using Steamworks;
 
@@ -9,6 +10,7 @@ public class MainMenu : MonoBehaviour
     /********** MARK: Variables **********/
     #region Variables
 
+    [SerializeField] TMP_Text debugText = null;
     [SerializeField] GameObject landingPagePanel = null;
 
     [SerializeField] bool useSteam = false;
@@ -34,7 +36,13 @@ public class MainMenu : MonoBehaviour
 
         if (useSteam) // if using steam prepare the lobby for the steam callbacks
         {
+            Debug.Log("starting SteamMatchmaking.CreateLobby");
+            debugText.text += ">starting SteamMatchmaking.CreateLobby\n";
+
             SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4); // HACK: hard-coded number of players
+
+            Debug.Log("completed SteamMatchmaking.CreateLobby");
+            debugText.text += ">completed SteamMatchmaking.CreateLobby\n";
         }
         else // otherwise, just create the host lobby
         {
@@ -49,13 +57,22 @@ public class MainMenu : MonoBehaviour
 
     private void SetupSteamCallbacks()
     {
+        Debug.Log("starting SetupSteamCallbacks");
+        debugText.text += ">starting SetupSteamCallbacks\n";
+
         lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
         gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
-        lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEnter);
+        lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+
+        Debug.Log("completed SetupSteamCallbacks");
+        debugText.text += ">completed SetupSteamCallbacks\n";
     }
 
     private void OnLobbyCreated(LobbyCreated_t callback)
     {
+        Debug.Log("starting OnLobbyCreated");
+        debugText.text += ">starting OnLobbyCreated\n";
+
         // TODO: idk what this if statement does, he didn't go over it
         if (callback.m_eResult != EResult.k_EResultOK)
         {
@@ -71,15 +88,27 @@ public class MainMenu : MonoBehaviour
             "HostAddress", // this is the key to get the lobby ID
             SteamUser.GetSteamID().ToString()
         );
+
+        Debug.Log("completed OnLobbyCreated");
+        debugText.text += ">completed OnLobbyCreated\n";
     }
 
     private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
     {
+        Debug.Log("starting OnGameLobbyJoinRequested");
+        debugText.text += ">starting OnGameLobbyJoinRequested\n";
+
         SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+
+        Debug.Log("completed OnGameLobbyJoinRequested");
+        debugText.text += ">completed OnGameLobbyJoinRequested\n";
     }
 
-    private void OnLobbyEnter(LobbyEnter_t callback)
+    private void OnLobbyEntered(LobbyEnter_t callback)
     {
+        Debug.Log("starting OnLobbyEntered");
+        debugText.text += ">starting OnLobbyEntered\n";
+
         if (NetworkServer.active) return; // do nothing if we're the server
 
         string hostAddress = SteamMatchmaking.GetLobbyData(
@@ -91,6 +120,9 @@ public class MainMenu : MonoBehaviour
         NetworkManager.singleton.StartClient();
 
         landingPagePanel.SetActive(false);
+
+        Debug.Log("completed OnLobbyEntered");
+        debugText.text += ">completed OnLobbyEntered\n";
     }
 
     #endregion
